ec7ee917592daaedb6bc6190dcbd0ffcf82b5be6
HabTeg21 <81588184+HabTeg21@users.noreply.github.com>
2024-02-01 22:30:13 -0600

    update the files

--- MyPortfolio/MyPortfolio.Server/obj/Debug/net8.0/MyPortfolio.Server.AssemblyInfo.cs
+++ MyPortfolio/MyPortfolio.Server/obj/Debug/net8.0/MyPortfolio.Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyPortfolio.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+598baf9d8ec81da5a9272c038607b003bd430779")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyPortfolio.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyPortfolio.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
