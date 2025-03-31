339b01530aeba40563194ace0127946e5aa3bccf
HabTeg21 <81588184+HabTeg21@users.noreply.github.com>
2025-03-31 00:37:16 -0500

    Committing my changes before pulling remote changes

--- MyPortfolio/MyPortfolio.Server/obj/Debug/net8.0/MyPortfolio.Server.AssemblyInfo.cs
+++ MyPortfolio/MyPortfolio.Server/obj/Debug/net8.0/MyPortfolio.Server.AssemblyInfo.cs
@@ -13,7 +13,11 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyPortfolio.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
+<<<<<<< HEAD
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d06db391bee576abd2fd470e2c69b6a41ea32b6f")]
+=======
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+>>>>>>> 7b2c9386f5f753c41720a1579ff75db630836203
 [assembly: System.Reflection.AssemblyProductAttribute("MyPortfolio.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyPortfolio.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
